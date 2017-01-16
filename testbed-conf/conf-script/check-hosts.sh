2d42ef075d8a18fc59f19439787128b02bc288da
frankfzw <frankfzw@gmail.com>
2017-01-16 11:52:23 +0800

    add script to ping all paths

--- testbed-conf/conf-script/check-hosts.sh
+++ testbed-conf/conf-script/check-hosts.sh
@@ -5,14 +5,23 @@ sbin=`cd "$sbin"; pwd`
 
 PINGS=`cat ${sbin}/ping_list`
 
-
+ARRAY=(10 20 30 40)
 
 for ip in $PINGS; do
 	if [[ "$ip" == \#* ]]; then
 		continue
 	fi
-	echo "ping $ip got"
+	echo "ping host $ip got"
 	echo `ping -c 5 -w 5 $ip | grep "packet loss"`
+
+	#ping path
+	IFS='.' read -a ids <<< "${ip}"
+	for pid in ${ARRAY[@]}; do
+		PIP="${ids[0]}.${ids[1]}.${ids[2]}.$((${ids[3]} + ${pid}))"
+		echo "ping path $PIP got"
+		echo `ping -c 5 -w 5 $PIP | grep "packet loss"`
+	done
+		
 done
 
 
